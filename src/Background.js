39f76da0b7b140fa24f74f071f8ca5b73da4977e
@SantSurat <santsurat123@gmail.com>
2023-05-04 12:06:43 +0530

    React.js

---
+++ src/Background.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+const Background  = ({children}) => {
+    return(
+
+        <Background source={require("./assets/imgA.jpg")} style={styles.container} />
+    
+    )
+}
+export default Background;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,39 @@
-import React from 'react'
-//import bgImg from '../assets/logors.gif';
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 export default function Form() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm()
-    const onSubmit = data => console.log(data);
+  // console.log(watch('username'));
 
-    // console.log(watch('username'));
-    
   return (
     <section>
-        <div className="register">
-            <div className="col-1">
-                <h2>Sign In</h2>
-                <span>ADANBAGH ATTEBDENCE MANAGEMENT SYSTEM</span>
+      <div className="register">
+        <div className="col-1">
+          <h2>Sign In</h2>
+          <span>ADANBAGH ATTENDENCE MANAGEMENT SYSTEM</span>
 
-                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                    <input type="text" {...register("username")} placeholder='username' />
-                    <input type="text" {...register("password")} placeholder='password' />
-                    <button className='btn'>Sign In</button>
-                </form>
-
-            </div>
+          <form
+            id="form"
+            className="flex flex-col"
+          >
+            <FontAwesomeIcon icon={faUser} className="Icon1" />
+            <input
+             type="email"
+            placeholder="Enter Email"
+            />
+            <FontAwesomeIcon icon={faLock} className="Icon1" />
+            <input
+              type="password"
+              placeholder="Password"
+              
+            />
+            <button className="btn">Sign In</button>
+          </form>
         </div>
+      </div>
     </section>
-  )
+  );
 }
